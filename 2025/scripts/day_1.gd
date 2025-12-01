293b9d470bfe52122daab1b3fd3677a73a6dec16
Rene Schwartz <rene.j.schwartz@gmail.com>
2025-12-01 15:25:04 +0200

    day 1 cleanup

--- 2025/scripts/day_1.gd
+++ 2025/scripts/day_1.gd
@@ -1,7 +1,11 @@
+class_name Day1
 extends Node
+## Combination lock is rotated left or right some amount and door password is 
+## deducted from how many times the lock lands on 0 or passes it.
+## Rotating a lock sprite takes a long ass time (1-20 min) and floating point 
+## errors made me do this with just regular math, but I got pretty close with 
+## rotation animations too.
 
-@export var combination_lock : Sprite2D
-var spin_time_per_degree = 0.00005
 var example_rotations : String = """L68
 L30
 R48
@@ -15,22 +19,16 @@ L82"""
 
 func _ready() -> void: 
 	var count_of_zeroes = 0
-	# starting location of 50 / 100
+	# Starting at 50 on the lock dial. This number is always between 0 and 99.
 	var location = 50
 	var part = 2
 	
 	# count how many times wheel lands on 0
 	if part == 1:
-		#await get_tree().create_tween().tween_property(combination_lock, "rotation_degrees", combination_lock.rotation_degrees + 3.6 * 50, spin_time_per_degree * 50).finished
-		#print(combination_lock.rotation_degrees / 3.6)s
-		# now to do other rotations after initial starting point at 50
 		for instruction in example_rotations.split("\n"):
-			var dir = 1
-			if instruction[0] == 'L':
-				dir = -1
+			var dir = 1 if instruction[0] == 'R' else -1
 			var amt = int(instruction.substr(1))
 			location += dir * amt
-			#await get_tree().create_tween().tween_property(combination_lock, "rotation_degrees", combination_lock.rotation_degrees + 3.6 * amt * dir, spin_time_per_degree * amt).finished
 			if location % 100 == 0:
 				count_of_zeroes += 1
 		print(count_of_zeroes)
@@ -38,17 +36,15 @@ func _ready() -> void:
 	# count how many times wheel clicks on 0
 	if part == 2:
 		for instruction in example_rotations.split("\n"):
-			var dir = 1
-			if instruction[0] == 'L':
-				dir = -1
+			var dir = 1 if instruction[0] == 'R' else -1
 			var amt = int(instruction.substr(1))
 			for x in range(amt):
 				location += dir
 				if location == -1:
-						location = 99
+					location = 99
 				elif location == 100:
 					location = 0
-					
+				
 				if location == 0:
-						count_of_zeroes += 1
+					count_of_zeroes += 1
 		print(count_of_zeroes)
